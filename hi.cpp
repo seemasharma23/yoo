56f539e8d2d64d29b1e2801c20b851f688631a65
seemasharma23 <seema.sharma1@s.amity.edu>
2025-05-25 00:47:07 +0530

    commitibg yoo1

---
+++ hi.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  double length = 5.5;
+  double width = 10.2;
+  double area = length * width;
+  std::cout << "The area of the rectangle is: " << area << std::endl;
+  return 0;
+}
